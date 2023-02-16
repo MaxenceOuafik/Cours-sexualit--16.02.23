67cfc29479a1c6afd7642770a658236ff6531d24
Maxence Ouafik <maxence.ouafik@outlook.com>
2023-02-16 05:36:49 +0100

    Création de la présentation

--- scripts/import_data.R
+++ scripts/import_data.R
@@ -3,7 +3,7 @@ library(tidyverse)
 # On récupère les données de patients_data.csv en spécifiant les colonnes devant être traitées comme logiques 
 # (vrai/faux) et quelles colonnes sont numériques. 
 
-patients_data <- read_delim("./data/patients_data.csv",
+patients_data <- read_delim(here::here("data/patients_data.csv"),
                             locale = locale(encoding = "UTF-8"),
                             col_types = cols(dmg = col_logical(),
                                              BIM = col_logical(),
@@ -13,12 +13,12 @@ patients_data <- read_delim("./data/patients_data.csv",
 patients_data$province <- as.factor(patients_data$province)
 
 # Importation des données de l'AIM en précisant que les décimales sont marquées par une virgule 
-aim_data <- read_delim("./data/AIM_data.csv",
+aim_data <- read_delim(here::here("data/AIM_data.csv"),
                        locale = locale(decimal_mark = ","))       
 
-refnis_postal <- read_delim("./data/refnis_postal.csv",
+refnis_postal <- read_delim(here::here("data/refnis_postal.csv"),
                             locale = locale(encoding = "UTF-8"))
 
-mg_survey <- read_delim("./data/MG_survey.csv")
+mg_survey <- read_delim(here::here("data/MG_survey.csv"))
 
-lgbt_survey <- read_delim("./data/LGBT_survey.csv")
+lgbt_survey <- read_delim(here::here("data/LGBT_survey.csv"))
---
+++ scripts/move_presentation.R
@@ -0,0 +1,2 @@
+file.rename(from = "./cours_presentation-speaker.html", 
+            to = "./docs/cours_presentation-speaker.html")
